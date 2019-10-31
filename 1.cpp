e58b8d34c2a3268676aa324ea8c114600dfb7faa
unknown <j.naveros@uniandes.edu.co>
2019-10-30 23:53:55 -0500

    Tarea 12

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+
+double diametro(double r);
+double perimetro(double r);
+double area(double r);
+double pi = acos(-1.0);
+
+// <Para poner comentarios>
+int main(void)
+{
+    std::cout<<"Ingrese el radio del circulo:"<<std::endl;
+    double R;
+    std::cin>>R;
+    std::cout<<"El diámetro es: "<<diametro(R)<<std::endl;
+    std::cout<<"El perímetro es: "<<perimetro(R)<<std::endl;
+    std::cout<<"El área es: "<<area(R)<<std::endl;
+    return 0;
+}
+
+double diametro(double r)
+{
+    return 2.0*r;
+}
+
+double perimetro(double r)
+{
+    return 2.0*pi*r;
+}
+
+double area(double r)
+{
+    return pi*r*r;
+}
---
+++ 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+bool triangulo(int L1, int L2, int L3);
+
+int main(void)
+{
+    std::cout<<"Ingrese la longitud de los lados del triangulo:"<<std::endl;
+    int L1;
+    int L2;
+    int L3;
+    std::cout<<"Lado 1:"<<std::endl;
+    std::cin>>L1;
+    std::cout<<"Lado 2:"<<std::endl;
+    std::cin>>L2;
+    std::cout<<"Lado 3:"<<std::endl;
+    std::cin>>L3;
+    if(triangulo(L1, L2, L3) == 1)
+    {
+        std::cout<<"¿Se puede formar un triangulo rectangulo? Sí"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"¿Se puede formar un triangulo rectangulo? No"<<std::endl;
+    }        
+    return 0;
+}
+
+bool triangulo(int L1, int L2, int L3)
+{
+    return (L1*L1 + L2*L2 == L3*L3) || (L1*L1 + L3*L3 == L2*L2) || (L2*L2 + L3*L3 == L1*L1);
+}
